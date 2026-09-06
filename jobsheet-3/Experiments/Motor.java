23c61118fe9017e9c4e619b59cd0f548d1cb6d18
rrworkscom <rrworks010@gmail.com>
2026-09-06 22:30:58 +0700

    add

---
+++ jobsheet-3/Experiments/Motor.java
@@ -0,0 +1,19 @@
+public class Motor {
+
+    public String licensePlate;
+    public boolean isMachineOn;
+    public int speed;
+
+    public void displayStatus() {
+        System.out.println("License Plate: " + this.licensePlate);
+
+        if (isMachineOn) {
+            System.out.println("Machine On");
+        } else {
+            System.out.println("Machine Off");
+        }
+
+        System.out.println("Speed:" + this.speed);
+        System.out.println("==================");
+    }
+}
---
+++ jobsheet-3/Experiments/MotorDemo.java
@@ -0,0 +1,20 @@
+public class MotorDemo {
+    public static void main(String[] args) {
+        Motor motor1 = new Motor();
+        motor1.displayStatus();
+        motor1.licensePlate = "B 0838 XZ";
+        motor1.speed = 50;
+        motor1.displayStatus();
+
+        Motor motor2 = new Motor();
+        motor2.licensePlate = "N 9840 AB";
+        motor2.isMachineOn = true;
+        motor2.speed = 40;
+        motor2.displayStatus();
+
+        Motor motor3 = new Motor();
+        motor3.licensePlate = "D 8343 CV";
+        motor3.speed = 60;
+        motor3.displayStatus();
+    }
+}
