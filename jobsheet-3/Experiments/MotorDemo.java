48d65aac17b6e90551ff8b339561c687626981ed
rrworkscom <rrworks010@gmail.com>
2026-09-06 22:39:52 +0700

    update

--- jobsheet-3/Experiments/MotorDemo.java
+++ jobsheet-3/Experiments/MotorDemo.java
@@ -3,18 +3,39 @@ public class MotorDemo {
         Motor motor1 = new Motor();
         motor1.displayStatus();
         motor1.licensePlate = "B 0838 XZ";
-        motor1.speed = 50;
+
+        int newSpeed = 50;
+        if(!motor1.isMachineOn && newSpeed > 0) {
+            System.out.println("The speed must not exceed 0 when the engine is off");
+        } else {
+            motor1.speed = newSpeed;
+        }
+
         motor1.displayStatus();
 
         Motor motor2 = new Motor();
         motor2.licensePlate = "N 9840 AB";
         motor2.isMachineOn = true;
-        motor2.speed = 40;
+        newSpeed = 40;
+
+        if(!motor2.isMachineOn && newSpeed > 0) {
+            System.out.println("The speed must not exceed 0 when the engine is off");
+        } else {
+            motor2.speed = newSpeed;
+        }
+
         motor2.displayStatus();
 
         Motor motor3 = new Motor();
         motor3.licensePlate = "D 8343 CV";
-        motor3.speed = 60;
+        newSpeed = 60;
+
+        if(!motor3.isMachineOn && newSpeed > 0) {
+            System.out.println("The speed must not exceed 0 when the engine is off");
+        } else {
+            motor3.speed = newSpeed;
+        }
+
         motor3.displayStatus();
     }
 }
