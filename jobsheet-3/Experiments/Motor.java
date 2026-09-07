5c795281880d62c882102bbf1d5206525dcf4935
rrworkscom <rrworks010@gmail.com>
2026-09-07 07:58:10 +0700

    modify

--- jobsheet-3/Experiments/Motor.java
+++ jobsheet-3/Experiments/Motor.java
@@ -2,7 +2,35 @@ public class Motor {
 
     private String licensePlate;
     private boolean isMachineOn;
-    private int speed;
+    private int speed; 
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public boolean isMachineOn() {
+        return isMachineOn;
+    }
+
+    public void setIsMachineOn(boolean isMachineOn) {
+        this.isMachineOn = isMachineOn;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        if(!this.isMachineOn && speed > 0) {
+            System.out.println("The speed must not exceed 0 when the engine is off");
+        } else {
+            this.speed = speed;
+        }
+    }
 
     public void displayStatus() {
         System.out.println("License Plate: " + this.licensePlate);
--- jobsheet-3/Experiments/MotorDemo.java
+++ jobsheet-3/Experiments/MotorDemo.java
@@ -1,41 +1,23 @@
 public class MotorDemo {
+
     public static void main(String[] args) {
         Motor motor1 = new Motor();
         motor1.displayStatus();
-        motor1.licensePlate = "B 0838 XZ";
 
-        int newSpeed = 50;
-        if(!motor1.isMachineOn && newSpeed > 0) {
-            System.out.println("The speed must not exceed 0 when the engine is off");
-        } else {
-            motor1.speed = newSpeed;
-        }
-
+        motor1.setLicensePlate("B 0838 XZ");
+        motor1.setSpeed(50);
         motor1.displayStatus();
 
         Motor motor2 = new Motor();
-        motor2.licensePlate = "N 9840 AB";
-        motor2.isMachineOn = true;
-        newSpeed = 40;
-
-        if(!motor2.isMachineOn && newSpeed > 0) {
-            System.out.println("The speed must not exceed 0 when the engine is off");
-        } else {
-            motor2.speed = newSpeed;
-        }
-
+        motor2.setLicensePlate("N 9840 AB");
+        motor2.setIsMachineOn(true);
+        motor2.setSpeed(40);
         motor2.displayStatus();
 
         Motor motor3 = new Motor();
-        motor3.licensePlate = "D 8343 CV";
-        newSpeed = 60;
-
-        if(!motor3.isMachineOn && newSpeed > 0) {
-            System.out.println("The speed must not exceed 0 when the engine is off");
-        } else {
-            motor3.speed = newSpeed;
-        }
-
+        motor3.setLicensePlate("D 8343 CV");
+        motor3.setSpeed(60);
         motor3.displayStatus();
+
     }
 }
