f36a300ee116b1b6c11b502967b99e0100e2be88
rrworkscom <rrworks010@gmail.com>
2026-09-07 08:09:43 +0700

    update

--- jobsheet-3/Experiments/DemoUser.java
+++ jobsheet-3/Experiments/DemoUser.java
@@ -1,5 +1,6 @@
 public class DemoUser {
     public static void main(String[] args) {
-        User user1 = new User();
+        User user1 = new User("annisa.nadya", "Annisa Nadya", "annisa.nadya@gmail.com");
+        user1.printInfo();
     }
 }
